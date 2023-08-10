d7161b9847fe877cede1a76c4d28c3a18b263d91
djaydevs <80023967+djaydevs@users.noreply.github.com>
2023-08-10 20:16:07 +0800

    Added styles to admin sidebar

--- src/app/(admin)/admin/layout.tsx
+++ src/app/(admin)/admin/layout.tsx
@@ -11,10 +11,10 @@ export default function AdminPortalLayout({
   return (
     <html>
       <body className="h-full relative">
-        <aside className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-80 bg-gray-900">
+        <aside className="hidden h-full md:flex md:w-20 lg:w-64 md:flex-col md:fixed md:inset-y-0 z-80 bg-background">
           <AdminSidebar />
         </aside>
-        <main className="md:pl-72 pb-10 bg-background">
+        <main className="md:pl-20 lg:pl-64 pb-10 bg-background">
           <Providers>
             <AdminNavbar />
           </Providers>
