8ed53314e817102096053bbf9057beb29afdadf3
djaydevs <80023967+djaydevs@users.noreply.github.com>
2023-09-21 11:09:19 +0800

    Minor changes

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 
 import { prisma } from "@/lib/prisma";
 
-export async function GET(req: Request): Promise<NextResponse | never | null> {
+export async function GET(req: Request): Promise<Response | never | null> {
   try {
     const users = await prisma.user.findMany();
 
