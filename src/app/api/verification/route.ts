524e7cfde8307847d085f09dfa71d3fcb2047f77
djaydevs <80023967+djaydevs@users.noreply.github.com>
2023-10-04 00:10:11 +0800

    fix: remove unnecesary imports

--- src/app/api/verification/route.ts
+++ src/app/api/verification/route.ts
@@ -1,26 +1,23 @@
-import { prisma } from "@/lib/prisma";
-import { Prisma } from "@prisma/client";
-import { Status } from "@prisma/client"
-import { verify } from "crypto";
-import { Verified } from "lucide-react";
 import { NextResponse, NextRequest } from "next/server";
 
-export async function POST(request:NextRequest) {
+import { prisma } from "@/lib/prisma";
+
+export async function POST(request: NextRequest) {
     try {
         const reqBody = await request.json()
-        const {token} = reqBody
+        const { token } = reqBody
         console.log(token)
 
-        const user = await prisma.verificationToken.findFirst ({
+        const user = await prisma.verificationToken.findFirst({
             where: {
                 token: token,
-                expiresAt: {gt: new Date()} //change into expiresAt
+                expiresAt: { gt: new Date() } //change into expiresAt
             }
         });
 
-        if(!user) {
-            return NextResponse.json({error: "Invalid Token"}, {status: 400});
-        } 
+        if (!user) {
+            return NextResponse.json({ error: "Invalid Token" }, { status: 400 });
+        }
 
         console.log(user);
 
@@ -35,11 +32,11 @@ export async function POST(request:NextRequest) {
         return NextResponse.json({
             message: "Email Verified Successfully!",
             success: true
-        })      
-        
-    } catch (error:any) {
-        return NextResponse.json({error: error.message},
-        {status: 500})
+        })
+
+    } catch (error: any) {
+        return NextResponse.json({ error: error.message },
+            { status: 500 })
     }
-    
+
 }
