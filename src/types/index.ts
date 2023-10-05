e3057e78d3d46595bea787bdf5558465ad68402d
djaydevs <80023967+djaydevs@users.noreply.github.com>
2023-10-06 00:32:46 +0800

    refactor: modify zod user schema

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,12 +3,18 @@ import { z } from "zod";
 export const userSchema = z.object({
     id: z.string(),
     name: z.string().nullish(),
-    email: z.string().email(),
+    email: z.string({
+        required_error: "Email field is required",
+        invalid_type_error: "This field must be in email format",
+    }).email(),
+    emailVerified: z.string().nullish(),
     createdAt: z.string().nullish(),
     updatedAt: z.string().nullish(),
     image: z.string().url().nullish(),
-    role: z.string(),
+    role: z.string({
+        required_error: "You need to select a role.",
+    }),
+    isVerified: z.boolean(),
 });
 
 export type User = z.infer<typeof userSchema>
-// export type user = User
