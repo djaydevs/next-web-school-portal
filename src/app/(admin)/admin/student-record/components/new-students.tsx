dee47032c0a7f36d1ffd0cb8ee3435946dbcb881
djaydevs <80023967+djaydevs@users.noreply.github.com>
2023-10-30 21:30:30 +0800

    chore: add useCSV button

--- src/app/(admin)/admin/student-record/components/new-students.tsx
+++ src/app/(admin)/admin/student-record/components/new-students.tsx
@@ -1,9 +1,32 @@
+"use client";
+
 import { FC } from "react";
+import UseCSV from "@usecsv/react";
+
+import { Button } from "@/components/ui/button";
+import Icons from "@/components/ui/icons";
 
 interface newStudentsProps {}
 
 const NewStudents: FC<newStudentsProps> = ({}) => {
-  return <div>New Students</div>;
+  const renderButton = (openModal: any) => {
+    return (
+      <Button onClick={openModal}>
+        <Icons.FilePlus2 className="mr-2" />
+        Import Data
+      </Button>
+    );
+  };
+
+  return (
+    <div>
+      <UseCSV
+        importerKey="d4dd9a93-3546-44f6-9cbf-4ba978759442"
+        user={{ userId: 12345 }}
+        render={(openModal) => renderButton(openModal)}
+      />
+    </div>
+  );
 };
 
 export default NewStudents;
