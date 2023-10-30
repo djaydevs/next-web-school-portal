e3e87acf28a5c4ef9bf04757c9e3b64f05149b6b
Jovie <119099153+codeHobi@users.noreply.github.com>
2023-10-30 18:18:54 +0800

    Subject and Faculty

--- src/app/api/subject/route.ts
+++ src/app/api/subject/route.ts
@@ -7,15 +7,25 @@ export async function POST(req: NextRequest) {
     try {
       const body = await req.json();
 
-      const addSubject = await prisma.subject.create({
+      const add = await prisma.subject.create({
         data: {
-          subjectCode: body.subjectCode,
           subjectName: body.subjectName,
           strandId: body.strandId,
+          semester: {
+            create: {
+              semesterInfo: body.semestre
+            },
+          },
+          schoolYear: {
+            create: {
+              yearStart: body.yearStart,
+              yearEnd: body.yearEnd,
+            },   
+          },                            
         },
       });
   
-      return NextResponse.json(addSubject, { status: 200 });
+      return NextResponse.json(add, { status: 200 });
   
     } catch ( error: any ) {
       return NextResponse.json({ message: error.message }, { status: 500 })      
