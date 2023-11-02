8812ff13a25c956a78d745df612beb4722f3510f
Jovie <119099153+codeHobi@users.noreply.github.com>
2023-11-02 17:54:32 +0800

    Faculty: New and Assigning

--- src/app/api/faculty/[facultyId]/route.ts
+++ src/app/api/faculty/[facultyId]/route.ts
@@ -10,17 +10,30 @@ interface contextProps {
 //Assign the faculty to the selected section and subject
 export async function PATCH(req: NextRequest, context: contextProps) {
     try {
-        const { params } = context
-        const body = await req.json()
-        
-        await prisma.facultyProfile.update({
-          where: { id: params.facultyId},
-          data: {
-            section: { connect: { id: body.sectionId } },
-            subjects: { connect: { id: body.subjectId } },
-            gradeLevel: { connect: { id: body.gradeLevelId } },
-          },
-        });
+      const body = await req.json();
+
+      const faculty = await prisma.facultyProfile.findUnique({ where: { id: body.facultyId } });
+      const gradeLevel = await prisma.gradeLevel.findUnique({ where: { id: body.gradeLevelId } });
+      const section = await prisma.section.findUnique({ where: { id: body.sectionId } });
+      const subject = await prisma.subject.findUnique({ where: { id: body.subjectId } });
+      
+      // Assign faculty to a grade level
+      await prisma.gradeLevel.update({
+        where: { id: body.gradeLevelId },
+        data: { faculty: { connect: { id: body.facultyId } } },
+      });
+      
+      // Assign faculty to a section
+      await prisma.section.update({
+        where: { id: body.sectionId },
+        data: { faculty: { connect: { id: body.facultyId } } },
+      });
+      
+      // Assign faculty to a subject
+      await prisma.subject.update({
+        where: { id: body.subjectId },
+        data: { faculty: { connect: { id: body.facultyId } } },
+      });      
     
         return NextResponse.json({ message: "Successfully assign"}, { status: 200});
     } catch (error: any) {
--- src/app/api/faculty/route.ts
+++ src/app/api/faculty/route.ts
@@ -1,11 +1,9 @@
 import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 
-
-// Assign a Section to the student
+// Get the list of faculty users
 export async function GET(req: NextRequest) {
-    try {
-      
+    try {      
         const faculty = await prisma.user.findMany({
             where: {
                 role: "faculty",                
@@ -21,6 +19,30 @@ export async function GET(req: NextRequest) {
       return NextResponse.json(faculty, { status: 200 });
   
     } catch ( error: any ) {
-      return NextResponse.json({ message: error.message }, { status: 500 })      
+      return NextResponse.json({ message: error.message }, { status: 500 });      
+    }
+}
+
+//Add a new faculty
+export async function POST(req: NextRequest) {
+    try {
+        const body = await req.json();
+
+        const addFaculty = await prisma.facultyProfile.create({
+            data: {
+                lastName: body.lastName,
+                firstName: body.firstName,
+                middleName: body.middleName,
+                age: body.age,
+                dateOfBirth: body.dateOfBirth,
+                gender: body.gender,
+                address: body.address,
+                contactNumber: body.contactNumber,
+            },
+        });
+
+        return NextResponse.json(addFaculty, { status: 200});
+    } catch (error: any) {
+        return NextResponse.json({ message: error.message }, { status: 500 });      
     }
 }
