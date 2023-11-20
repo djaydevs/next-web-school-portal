91e7b75dfdb68fb610b76f5abcff522c474e7230
Jovie <119099153+codeHobi@users.noreply.github.com>
2023-11-20 17:34:38 +0800

    Chore: Dashboard cards

--- src/app/(admin)/admin/(dashboard)/components/dash-cards.tsx
+++ src/app/(admin)/admin/(dashboard)/components/dash-cards.tsx
@@ -1,4 +1,5 @@
 import { FC } from "react";
+import { useQuery } from "@tanstack/react-query";
 
 import {
   Card,
@@ -11,19 +12,42 @@ import {
 interface DashboardCardsProps {}
 
 const DashboardCards: FC<DashboardCardsProps> = ({}) => {
+  const { data: studentData, isError: studentError, error: studentErrorDetails } = useQuery({
+    queryKey: ["dashboardStudentCounts"],
+    queryFn: async () => {
+      const response = await fetch("/api/student");
+      if (!response.ok) {
+        throw new Error(`Error: ${response.statusText}`);
+      }
+      return response.json();
+    },
+  });
+
+  const { data: facultyData, isError: facultyError, error: facultyErrorDetails } = useQuery({
+    queryKey: ["dashboardFacultyCounts"],
+    queryFn: async () => {
+      const response = await fetch("/api/faculty");
+      if (!response.ok) {
+        throw new Error(`Error: ${response.statusText}`);
+      }
+      return response.json();
+    },
+  });
+
+  if (studentError || facultyError) {
+    return (
+      <div>
+        <span>Error fetching data:</span>
+        <pre>{studentError ? studentErrorDetails!.message : facultyErrorDetails!.message}</pre>
+      </div>
+    );
+  }
+
+  const studentCount = studentData?.length || 0;
+  const facultyCount = facultyData?.length || 0;
+
   return (
     <div className="flex w-full gap-4">
-      <Card className="w-full">
-        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-          <CardTitle className="text-sm font-medium">New Students</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <div className="text-2xl font-bold">85</div>
-          {/* <p className="text-xs text-muted-foreground">
-            +20.1% from last month
-          </p> */}
-        </CardContent>
-      </Card>
       <Card className="w-full">
         <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
           <CardTitle className="text-sm font-medium">
@@ -31,7 +55,7 @@ const DashboardCards: FC<DashboardCardsProps> = ({}) => {
           </CardTitle>
         </CardHeader>
         <CardContent>
-          <div className="text-2xl font-bold">1230</div>
+          <div className="text-2xl font-bold">{studentCount}</div>
           {/* <p className="text-xs text-muted-foreground">
             +180.1% from last month
           </p> */}
@@ -39,19 +63,10 @@ const DashboardCards: FC<DashboardCardsProps> = ({}) => {
       </Card>
       <Card className="w-full">
         <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-          <CardTitle className="text-sm font-medium">Alumni</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <div className="text-2xl font-bold">5,234</div>
-          {/* <p className="text-xs text-muted-foreground">+19% from last month</p> */}
-        </CardContent>
-      </Card>
-      <Card className="w-full">
-        <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
           <CardTitle className="text-sm font-medium">Faculty Members</CardTitle>
         </CardHeader>
         <CardContent>
-          <div className="text-2xl font-bold">55</div>
+          <div className="text-2xl font-bold">{facultyCount}</div>
           {/* <p className="text-xs text-muted-foreground">+201 since last hour</p> */}
         </CardContent>
       </Card>
