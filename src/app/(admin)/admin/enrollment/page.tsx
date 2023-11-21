c1fbe203e1507086450331860e4abded217f685a
djaydevs <80023967+djaydevs@users.noreply.github.com>
2023-11-21 21:49:38 +0800

    chore: filter students with enrollment only

--- src/app/(admin)/admin/enrollment/page.tsx
+++ src/app/(admin)/admin/enrollment/page.tsx
@@ -2,21 +2,16 @@
 
 import { FC } from "react";
 import { useQuery } from "@tanstack/react-query";
-import { useRouter } from "next/navigation";
 
 import { fetchStudent } from "@/hooks/getUsers";
 import { SkeletonTable } from "@/components/loading";
 import { columns } from "@/components/enrollment-columns";
-import { useToast } from "@/components/ui/use-toast";
-import { Student, studentSchema } from "@/types";
+import { Student } from "@/types";
 import EnrollmentTable from "./components/enrollment-table";
 
 interface UpdateEnrollmentProps {}
 
 const UpdateEnrollment: FC<UpdateEnrollmentProps> = ({}) => {
-  const router = useRouter();
-  const { toast } = useToast();
-
   const {
     data: students,
     isPending: isLoadingStudentsTable,
@@ -31,10 +26,21 @@ const UpdateEnrollment: FC<UpdateEnrollmentProps> = ({}) => {
     return <span>Error: {error.message}</span>;
   }
 
+  // show the students that has enrollment only
+  const studentsFilter = students?.filter(
+    (student) =>
+      student &&
+      student.studentProfile &&
+      student.studentProfile.enrollment &&
+      student.studentProfile.enrollment.length > 0,
+  ) as Student[];
+
   return (
     <div className="h-full flex-1 flex-col space-y-4 px-4 md:flex">
       <div>
-        <h2 className="text-2xl font-bold tracking-tight">Manage Enrollment for Incoming Grade 12</h2>
+        <h2 className="text-2xl font-bold tracking-tight">
+          Manage Enrollment for Incoming Grade 12
+        </h2>
         <p className="text-muted-foreground">
           Here&apos;s a list of student enrollment you can manage as a portal
           admin/registrar.
@@ -43,10 +49,10 @@ const UpdateEnrollment: FC<UpdateEnrollmentProps> = ({}) => {
       {isLoadingStudentsTable ? (
         <SkeletonTable />
       ) : (
-        <EnrollmentTable columns={columns} data={students} />
+        <EnrollmentTable columns={columns} data={studentsFilter} />
       )}
     </div>
   );
-}
+};
 
-export default UpdateEnrollment
+export default UpdateEnrollment;
